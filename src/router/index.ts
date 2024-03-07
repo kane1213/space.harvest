35797c4dce562b9f5817320db830eb9939e6c139
kane1213 <kane711213@gmail.com>
2024-03-07 15:00:57 +0800

    add envMode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home/index.vue'
-const base = '/space.harvest/'
+import _ from 'lodash'
+
+// alert(process.env.NODE_ENV)
+const mode = _.get(import.meta, 'env.MODE', 'development')
+
+const base = mode === 'development' ? '/' : '/space.harvest/'
 const routes = [
-  {
-    path: '/',
-    redirect: { name: 'Home' },
-  },
+  // {
+  //   path: '/',
+  //   redirect: { name: 'Home' },
+  // },
   { path: base, name: 'home', component: Home },
   {
     path: base + 'gallery',
