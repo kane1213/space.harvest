1c6743eb2f613c96122d04df0514d0e0a42d2178
kane1213 <kane711213@gmail.com>
2024-03-07 17:04:18 +0800

    add env for vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
-  },
-  base: process.env.NODE_ENV === 'production' ? '/space.harvest/' : './',
-})
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+    },
+    define: {
+      VITE_ENV: '',
+      'import.meta.env.MODE': mode,
+    },
+    base: process.env.NODE_ENV === 'production' ? '/space.harvest/' : './',
+  })
+}
