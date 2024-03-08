aba9cfb3c8057e34eb37900e332d6f1eed76acdc
kane1213 <kane711213@gmail.com>
2024-03-08 11:17:27 +0800

    remove rdudnent

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,17 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home/index.vue'
 import _ from 'lodash'
 
-// alert(process.env.NODE_ENV)
 const mode = _.get(window, 'EnvMode', 'production')
+console.log({ mode })
+// const mode = 'production'
 
 const base = mode === 'development' ? '/' : '/space.harvest/'
 const routes = [
-  ...(mode === 'production'
-    ? {
-        path: '/',
-        redirect: { name: 'Home' },
-      }
-    : {}),
+  // ...(mode === 'production'
+  //   ? {
+  //       path: '/',
+  //       redirect: { name: 'Home' },
+  //     }
+  //   : {}),
   { path: base, name: 'home', component: Home },
   {
     path: base + 'gallery',
